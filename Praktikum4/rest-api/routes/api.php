a3207f421ad669b8420d2d0504950d76cf98f84f
nuraini2502 <rainii2502@gmail.com>
2023-11-02 22:39:05 +0700

    Praktikum5

--- Praktikum4/rest-api/routes/api.php
+++ Praktikum4/rest-api/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AnimalController;
+# mengimport controller Student
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +21,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Method GET
+# Route animals
 Route::get('/animals', [AnimalController::class, 'index']);
-
-// Method POST
 Route::post('/animals', [AnimalController::class, 'store']);
-
-// Method PUT
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
-// Method DELETE
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# Route students
+# Method GET
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+
+# Method POST
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
