89762d00972d2b1c27180ca9ca79cc3d183a18f7
nuraini2502 <rainii2502@gmail.com>
2023-12-19 23:18:12 +0700

    Tugas dan Praktikum 11

---
+++ node/Praktikum11/task/data/students.js
@@ -0,0 +1,7 @@
+// TODO 1: Buat data students
+// code here
+let students = ["Tria Mauliada", "Yasmin Artamevia", "Muyasyarotus Zahra", "Nur Sayahidah","Pak Amir"];
+
+// TODO 2: export data students
+// code here
+module.exports = students;
